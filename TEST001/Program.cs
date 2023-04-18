4e3df4c866001f3d0ba2407b1a6c2542168a3051
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-18 22:42:47 +0300

    TEST folder

---
+++ TEST001/Program.cs
@@ -0,0 +1,22 @@
+﻿// string test = "12345\r6789";
+// Console.WriteLine(test);
+
+
+// string test = "12345\n6789";
+// Console.WriteLine(test);
+
+string test = "12345\r\n6789";
+Console.WriteLine(test);
+
+
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
