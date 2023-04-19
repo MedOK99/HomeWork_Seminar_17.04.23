1d0d17a8127acb009e713b436ec1ee8ace3bd81f
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-04-19 18:15:46 +0300

    готовое решение задачи 58 с комментариями

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -6,3 +6,75 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+Console.Clear();
+int m = ReadNumberFromConsole("Введите требуемое количество строк первой матрицы:");
+int n = ReadNumberFromConsole("Введите требуемое количество столбцов первой матрицы (являющееся количеством строк второй матрицы):");
+int p = ReadNumberFromConsole("Введите требуемое количество столбцов второй матрицы:");
+
+int[,] firstMartrix = new int[m, n];                    // объявляю новый массив первой матрицы
+FillArrayRandomNumbers(firstMartrix);                   // заполняю первый массив псевдослучайными числами методом FillArrayRandomNumbers
+Console.WriteLine($"Первая матрица:\n");
+PrintArray(firstMartrix);                               // вывод на печать первой матрицы
+
+int[,] secondMartrix = new int[n, p];                   // объявляю новый массив второй матрицы
+FillArrayRandomNumbers(secondMartrix);                  // заполняю второй массив псевдослучайными числами методом FillArrayRandomNumbers
+Console.WriteLine($"\nВторая матрица:\n");
+PrintArray(secondMartrix);                              // вывод на печать второй матрицы
+
+int[,] resultMatrix = new int[m, p];                     // объявляю новый массив, который будет результатом произведения двух предыдущих
+
+MatrixMultipl(firstMartrix, secondMartrix, resultMatrix);// запускаю метод перемножения матриц
+Console.WriteLine($"\nПроизведение первой и второй матриц:\n");
+PrintArray(resultMatrix);                                // вывод на печать матрицы произведений
+
+void MatrixMultipl(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix) // метод перемножения матриц
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)    // цикл пока i меньше длины строки первой матрицы
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)  // цикл пока j меньше длины столбцов первой матрицы
+    {
+      {
+        int multipl = 0;
+        for (int k = 0; k < firstMartrix.GetLength(1); k++)
+        {
+          multipl += firstMartrix[i, k] * secondMartrix[k, j];
+        }
+        resultMatrix[i, j] = multipl;
+      }
+    }
+  }
+}
+
+void FillArrayRandomNumbers(int[,] array)               // метод для наполнения двумерного массива псевдослучайными числами
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+int ReadNumberFromConsole(string message = "")          // метод для чтения из консоли введённого значения
+{
+  if (message != "")
+    Console.WriteLine(message);
+  string input = Console.ReadLine();
+  return int.Parse(input);
+}
+
+void PrintArray(int[,] array)   // метод для вывода массива на печать (с табуляцией)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    Console.Write("[      ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i, j]}\t");  // табуляция
+    }
+    Console.Write("]\n");       // добавил "\n" чтобы убрать строку: Console.WriteLine();
+  }
+}
+
